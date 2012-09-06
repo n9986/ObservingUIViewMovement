d5d37e35d4a925940e443d630e303b95f5cfe84b
Nandeep mali <nandeep@miniorb.in>
2012-09-06 15:42:43 +0530

    Initial Commit

---
+++ ObservingUIViewMovement/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ObservingUIViewMovement
+//
+//  Created by Nandeep Mali on 06/09/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
